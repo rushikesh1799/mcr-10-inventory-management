0af74c005a736f802316a0703d864409501808cb
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-08-12 21:39:12 +0530

    Update: added file

--- src/Components/Departments.jsx
+++ src/Components/Departments.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navigation from "./Navigation";
 
 import "./styles.css";
+import { useNavigate } from "react-router";
+import { DataContext } from "../Contexts/DataContext";
 
 const Departments = () => {
+    const { setFilters } = useContext(DataContext);
+    const navigate = useNavigate();
+
     return (
         <div className="departments_wrapper">
             <Navigation />
             <div className="dept_cards_container">
-                <div className="dept">Kitchen</div>
-                <div className="dept">Clothing</div>
-                <div className="dept">Toys</div>
+                <div
+                    className="dept"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "kitchen",
+                        }));
+                    }}
+                >
+                    Kitchen
+                </div>
+                <div
+                    className="dept"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "clothing",
+                        }));
+                    }}
+                >
+                    Clothing
+                </div>
+                <div
+                    className="dept"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "toys",
+                        }));
+                    }}
+                >
+                    Toys
+                </div>
             </div>
         </div>
     );
